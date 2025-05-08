94d5413a887582f173f259c0bf25930389303d96
권석찬 <harry@gwonseogchan-ui-noteubug.local>
2025-05-09 01:05:04 +0900

    fix title

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,8 @@ const notoSansKR = Noto_Sans_KR({
 });
 
 export const metadata = {
-  title: "주니어 기획자 권석찬 포트폴리오",
-  description:
-    "고객의 필요를 선제적으로 파악하고, 적시에 실현가능한 형태로 제공하겠습니다.",
+  title: "권석찬 SM엔터테인먼트 포트폴리오",
+  description: "권석찬 SM엔터테인먼트 포트폴리오",
 };
 
 export default function RootLayout({ children }) {
